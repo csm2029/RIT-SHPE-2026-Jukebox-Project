79e9f222c0cd36b0f679f3f9169524bd9bffe88f
ast7673 <atenesaca101@gmail.com>
2026-03-19 14:00:49 -0400

    added curr in the queue to keep track where in the queue you are

--- jukebox-backend/audio.py
+++ jukebox-backend/audio.py
@@ -9,6 +9,7 @@ class Queue:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.curr = None
         self.size = 0
 
     def enqueue(self, data):
@@ -26,17 +27,23 @@ class Queue:
         # Set the prev of the new tail to the prev node
         self.tail.prev = prev_node
 
-        # If the queue is empty
+        # If the queue is empty, have the curr node point to the head
         if self.head is None:
             self.head = new_node
+            self.curr = new_node
         self.size += 1
 
     def next_node(self):
-        curr = self.head
+        curr = self.curr
+        curr = curr.next
+        self.curr = curr
 
-        while curr:
-            print(curr.data)
-            curr = curr.next
+        # Testing purposes
+        print(curr.data)
+
+    def prev_node(self):
+        curr = self.head
+        pass
 
 song_ex1 = { "name": "Quiere", "file_path": "beep boop...", "artist": "dei v", "album": "Los Flavorz" }  
 song_ex2 = { "name": "Toa", "file_path": "beep boop...", "artist": "dei v", "album": "Los Flavorz" }
@@ -49,5 +56,8 @@ def test():
     jukebox.enqueue(song_ex3)
 
     jukebox.next_node()
+    jukebox.next_node()
+
+    #jukebox.prev_node()
 
 test()
